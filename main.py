90e081d049f72520d251b9d2ddaef53e9a97ae68
Sergey Galich <9985613@gmail.com>
2019-06-02 16:32:12 +0300

    still far from redy

--- main.py
+++ main.py
@@ -6,39 +6,41 @@ import logging
 from datetime import datetime
 import time
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-					level=logging.INFO,
-					filename='test.log'
-					)
+def main():
+	logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+						level=logging.INFO,
+						filename='test.log'
+						)
 
-wrp=ibapi.wrapper.EWrapper()
-cln=ibapi.client.EClient(wrp)
-
-
-cln.connect("127.0.0.1", 7497, 1)
-
-# не понимаю как работать с потоком. ИЗУЧИТЬ модуль threading!!
-if cln.isConnected():
-	print("Успешно подключились к TWS")
-	cln.th = threading.Thread(target=cln.run())
-	cln.th.start()
-	cln.th.join(timeout=5)
-	time_now_for_log=datetime.now().strftime("%m/%d/%Y %I:%M %p")
-	logging.info(f"Time: {time_now_for_log}, status: {cln.th.join()}")
-
-#ibapi.client.EClient.run() # что за метод? ИЗУЧИТЬ!
-
-contract = ibapi.contract.Contract()
-contract.symbol = "IBKR"
-contract.secType = "STK"
-contract.currency = "USD"
-contract.exchange = "ISLAND"
+	wrp=ibapi.wrapper.EWrapper()
+	cln=ibapi.client.EClient(wrp)
 
 
-time.sleep(60*60*24)
-cln.disconnect()
+	cln.connect("127.0.0.1", 7497, 1)
+
+	# не понимаю как работать с потоком. ИЗУЧИТЬ модуль threading!!
+	if cln.isConnected():
+		print("Успешно подключились к TWS")
+		cln.th = threading.Thread(target=cln.run())
+		cln.th.start()
+		cln.th.join(timeout=5)
+		time_now_for_log=datetime.now().strftime("%m/%d/%Y %I:%M %p")
+		logging.info(f"Time: {time_now_for_log}, status: {cln.th.join()}")
+
+	#ibapi.client.EClient.run() # что за метод? ИЗУЧИТЬ!
+
+	contract = ibapi.contract.Contract()
+	contract.symbol = "IBKR"
+	contract.secType = "STK"
+	contract.currency = "USD"
+	contract.exchange = "ISLAND"
 
 
+	time.sleep(60*60*24)
+	cln.disconnect()
+
+if __name__=='__main__':
+	main()
 
 
 
