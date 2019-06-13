add5ead7c752a61f9be4c2e6f4b4848b99c69516
Sergey Galich <9985613@gmail.com>
2019-06-13 15:44:36 +0300

    +TP and SL

--- updater.py
+++ updater.py
@@ -20,7 +20,7 @@ def data_adding(new_price_data, stock_ticker):
 	for row in new_price_data:
 		if last_date in row:
 			i = new_price_data.index(row)	# new prices cince this index in new_price_data
-	if i == -1:
+	if i == -1 and new_price_data != []:
 		print(f'{stock_ticker} needs in updating with more depth. Last data was collected: {last_date}')
 		exit()
 	with open(f'historical_data/{stock_ticker}.csv', 'a', encoding='utf-8') as data_file:
@@ -34,8 +34,8 @@ def error_handler(msg):
 	if msg.errorCode == 2104 or msg.errorCode == 2106:
 		pass
 	elif msg.errorCode == 2103 or msg.errorCode == 504:
-		print('CONNECTION ERROR!')
 		print(msg)
+		print('CONNECTION ERROR!')
 		exit()
 	else:
 		print(msg)
@@ -48,7 +48,7 @@ def requesting(conn, stock_ticker):
 	conn.reqHistoricalData(1,	# tickerId, A unique identifier which will serve to identify the incoming data.
 							my_contract,	# your Contract()
 							'',	# endDateTime, The request's end date and time (the empty string indicates current present moment)
-							'1 D',	# durationString, S D W M Y (seconds, days, weeks, months, year)
+							'2 D',	# durationString, S D W M Y (seconds, days, weeks, months, year)
 							BAR_SIZE,	# barSizeSetting, 1,5,10,15,30secs, 1,2,3,5,10,15,20,30min[s], 1,2,3,4,8hour[s], 1day,week,month
 							"TRADES",	# whatToShow
 							1,	# useRTH, Whether (1) or not (0) to retrieve data generated only within Regular Trading Hours (RTH)
@@ -79,6 +79,6 @@ def main(conn, set_of_companies):
 if __name__ == '__main__':
 	c = Connection.create(port=7497, clientId=0)
 	c.connect()
-	main(c, {'SYY'})
+	main(c, {'MOMO'})
 	c.disconnect()
 
