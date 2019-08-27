8ce97cfd012cf4729b17787354d165df4141d0a1
Sergey Galich <9985613@gmail.com>
2019-08-27 16:25:10 +0300

    -

--- signals.py
+++ signals.py
@@ -22,8 +22,7 @@ def check(price_data, strategy_indicators):
 		ind_signal = getattr(globals()[f'{indicator}'], 'signal')(price_data, strategy_indicators[f'{indicator}'])
 		ind_weight = strategy_indicators[f'{indicator}']['weight']
 		signal += ind_signal * ind_weight
-		if indicator == 'stochastic' and signal < 0:
-			print(price_data[-1]['Datetime'], signal)
+		print(price_data[-1]['Datetime'], indicator, signal)
 	quantity_of_indicators = len(all_indicators)
 	if signal >= quantity_of_indicators:
 		# print(price_data[-1]['Datetime'], signal, 'buy')
